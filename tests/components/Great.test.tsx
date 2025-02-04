0f56dd2924533ecc0d495a396067298a14679a7b
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-02-04 20:06:25 +0330

    Simplifying test setup

--- tests/components/Great.test.tsx
+++ tests/components/Great.test.tsx
@@ -1,7 +1,6 @@
 import { it, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Greet from '../../src/components/Greet';
-import '@testing-library/jest-dom/vitest';
 
 describe('Greet Component', () => {
 	it('should render hello with the name, when name is provided', () => {
