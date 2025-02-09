438c1afe3ceed6af92d8c94075db262a1b0c708c
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-02-09 22:20:42 +0330

    testing SearchBox

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,15 @@
 // import Onboarding from "../components/Onboarding";
-import TermsAndConditions from "../components/TermsAndConditions";
 
+import SearchBox from '../components/SearchBox';
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+	return (
+		<SearchBox
+			onChange={text => {
+				console.log(text);
+			}}
+		/>
+	);
 };
 
 export default PlaygroundPage;
---
+++ tests/components/SearchBox.test.tsx
@@ -0,0 +1,39 @@
+import { it, expect, vi, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import SearchBox from '../../src/components/SearchBox';
+import userEvent from '@testing-library/user-event';
+
+describe('SearchBox', () => {
+	const renderSearchBox = () => {
+		const onChange = vi.fn();
+		render(<SearchBox onChange={onChange} />);
+
+		return {
+			input: screen.getByPlaceholderText(/search/i),
+			user: userEvent.setup(),
+			onChange,
+		};
+	};
+
+	it('should render an input field for searching', () => {
+		const { input } = renderSearchBox();
+		expect(input).toBeInTheDocument();
+	});
+
+	it('should call onChange when Enter is pressed', async () => {
+		const { onChange, input, user } = renderSearchBox();
+
+		const searchTerm = 'SearchTerm';
+		await user.type(input, searchTerm + '{enter}');
+
+		expect(onChange).toHaveBeenCalledWith(searchTerm);
+	});
+
+	it('should not call onChage if input field is empty', async () => {
+		const { onChange, input, user } = renderSearchBox();
+
+		await user.type(input, '{enter}');
+
+		expect(onChange).not.toHaveBeenCalled();
+	});
+});
