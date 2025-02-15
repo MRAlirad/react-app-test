d0c3db53006c81cbc2c1c315c33783860c79b389
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-02-15 22:24:50 +0330

    Setting up MSW

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,10 @@
 import { describe, it, expect } from 'vitest';
 
 describe('Truthy Test', () => {
-    it('should verify that 1 is truthy', () => {
-        expect(1).toBeTruthy();
-    });
+	it('should verify that 1 is truthy', async () => {
+		const response = await fetch('/categories');
+		const data = await response.json();
+		console.log(data);
+		expect(data).toHaveLength(3);
+	});
 });
