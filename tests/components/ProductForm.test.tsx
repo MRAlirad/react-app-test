b47095784150dc4b309229b7983326cfd988cd42
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-03-01 17:19:06 +0330

    product form - refactoring tests

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -16,15 +16,36 @@ describe('ProductForm', () => {
 		db.category.delete({ where: { id: { equals: category.id } } });
 	});
 
+	const renderCompnent = (product?: Product) => {
+		render(
+			<ProductForm
+				product={product}
+				onSubmit={vi.fn()}
+			/>,
+			{ wrapper: AllProviders }
+		);
+
+		return {
+			waitForFormToLoad: () => screen.findByRole('form'),
+			getInputs: ()=> {
+				return {
+					nameInput: screen.getByPlaceholderText(/name/i),
+					priceInput: screen.getByPlaceholderText(/price/i),
+					categoryInput: screen.getByRole('combobox', { name: /category/i }),
+				}
+			},
+		};
+	};
+
 	it('should render the form field', async () => {
-		render(<ProductForm onSubmit={vi.fn()} />, { wrapper: AllProviders });
+		const { waitForFormToLoad, getInputs } = renderCompnent();
+		await waitForFormToLoad();
+		
+		const {nameInput, priceInput, categoryInput} = getInputs();
 
-		await screen.findByRole('form');
-		// await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
-
-		expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument();
-		expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument();
-		expect(screen.getByRole('combobox', { name: /category/i })).toBeInTheDocument();
+		expect(nameInput).toBeInTheDocument();
+		expect(priceInput).toBeInTheDocument();
+		expect(categoryInput).toBeInTheDocument();
 	});
 
 	it('should populate form fields when editing a product', async () => {
@@ -35,19 +56,13 @@ describe('ProductForm', () => {
 			categoryId: category.id,
 		};
 
-		render(
-			<ProductForm
-				product={product}
-				onSubmit={vi.fn()}
-			/>,
-			{ wrapper: AllProviders }
-		);
+		const { waitForFormToLoad, getInputs } = renderCompnent(product);
+		await waitForFormToLoad();
+		
+		const {nameInput, priceInput, categoryInput} = getInputs();
 
-		await screen.findByRole('form');
-		// await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
-
-		expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name);
-		expect(screen.getByPlaceholderText(/price/i)).toHaveValue(product.price.toString());
-		expect(screen.getByRole('combobox', { name: /category/i })).toHaveTextContent(category.name);
+		expect(nameInput).toHaveValue(product.name);
+		expect(priceInput).toHaveValue(product.price.toString());
+		expect(categoryInput).toHaveTextContent(category.name);
 	});
 });
